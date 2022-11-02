15cfc4543866991f321b1e4114ab92844520a97b
HardManDev <hardman.dev@protonmail.com>
2022-11-02 19:52:06 +0300

    feat: complete task "02-write-file"

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const writeStream = fs.createWriteStream(
+  path.resolve(__dirname, './text.txt')
+);
+const readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readlineInterface.setPrompt('Enter string (or type "exit" for exit): ');
+
+readlineInterface.on('line', (line) => {
+  if (line === 'exit') {
+    readlineInterface.close();
+    return;
+  }
+
+  writeStream.write(`${line}\n`);
+});
+readlineInterface.on('SIGINT', readlineInterface.close);
+
+readlineInterface.on('close', () => {
+  console.log('Process exit with code 0');
+});
+
+
+readlineInterface.prompt();
