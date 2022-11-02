ffc837c818c42b58e0471809dd99272919ce7cb6
HardManDev <hardman.dev@protonmail.com>
2022-11-03 00:16:41 +0300

    feat: complete task "03-files-in-folder"

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const targetFolderPath = path.resolve(__dirname, './secret-folder/');
+
+fs.readdir(
+  targetFolderPath,
+  {
+    withFileTypes: true
+  },
+  (err, files) => {
+    files.forEach(file => {
+      fs.stat(path.resolve(targetFolderPath, file.name), (err, stats) => {
+        const fileName = path.parse(file.name).name;
+        const fileExt = path.parse(file.name).ext.replace('.', '');
+        const fileSize = stats.size;
+
+        if (stats.isFile())
+          console.log(`${fileName} - ${fileExt} - ${fileSize / 1024}KB`);
+      });
+    });
+  }
+);
