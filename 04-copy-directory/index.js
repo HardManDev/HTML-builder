413fa4dbae4bd31781a280295b1718b1cf65a658
HardManDev <hardman.dev@protonmail.com>
2022-11-04 00:29:11 +0300

    feat: complete task "04-copy-directory"

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyFile = (fromPath, toPath) => {
+  fs.rm(toPath, {
+    recursive: true
+  }, () => {
+    fs.mkdir(toPath, () => {
+      fs.readdir(fromPath,
+        {
+          withFileTypes: true
+        },
+        (err, files) => {
+          files.forEach((file) => {
+            const writeStream = fs.createWriteStream(
+              path.resolve(toPath, file.name)
+            );
+            const readStream = fs.createReadStream(
+              path.resolve(fromPath, file.name)
+            );
+
+            readStream.pipe(writeStream);
+          });
+        });
+    });
+  });
+};
+
+copyFile(
+  path.resolve(__dirname, 'files'),
+  path.resolve(__dirname, 'file-copy')
+);
