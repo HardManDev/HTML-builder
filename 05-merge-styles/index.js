d987c20e2ce7dbe886ba60c4a146f2c496096197
HardManDev <hardman.dev@protonmail.com>
2022-11-07 19:33:01 +0300

    fix: fix task "05-merge-styles"

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,8 +3,29 @@ const path = require('path');
 
 const sourceFilesPath = path.resolve(__dirname, './styles');
 const writeStream = fs.createWriteStream(
-  path.resolve(__dirname, './project-dist', 'bundle.css')
+  path.resolve(__dirname, './project-dist', 'bundle.css'),
+  'utf-8'
 );
+
+/**
+ * @param {string} path
+ * @returns {Promise<string>}
+ */
+const readTextFileData = (path) => {
+  return new Promise(resolve => {
+    let data = '';
+    const readStream = fs.createReadStream(path, 'utf-8');
+
+    readStream.on('data', (chunk) => {
+      data += chunk.toString();
+    });
+
+    readStream.on('end', () => {
+      resolve(data);
+    });
+  });
+};
+
 
 fs.readdir(
   sourceFilesPath,
@@ -12,10 +33,13 @@ fs.readdir(
     withFileTypes: true
   },
   (err, files) => {
-    files.forEach(file => {
-      if (file && path.extname(file.name) === '.css')
-        fs.createReadStream(path.resolve(sourceFilesPath, file.name))
-          .pipe(writeStream, { end: false });
+    files.forEach(async file => {
+      if (file && path.extname(file.name) === '.css') {
+        const str = await readTextFileData(
+          path.resolve(sourceFilesPath, file.name)
+        );
+        writeStream.write(`${str}\n`);
+      }
     });
   }
 );
